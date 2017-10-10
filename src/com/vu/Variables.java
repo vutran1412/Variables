311e4575c921c0c59219ad62d1e3a1ce7f35a452
vutran1412 <vutran1412@icloud.com>
2017-10-10 00:13:59 -0500

    Initial commit

---
+++ src/com/vu/Variables.java
@@ -0,0 +1,45 @@
+package com.vu;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int intNumber = 6;
+        double doubleNumber = 6.1234;
+
+        String school = "MCTC";
+        String language = "Java";
+
+        int addNumber = intNumber + 10;
+
+        int size = 12;
+        size = 24;
+
+        double multiplyNumber = doubleNumber * 100;
+
+        System.out.println(intNumber);
+        System.out.println(doubleNumber);
+        System.out.println(school);
+        System.out.println(language);
+
+        System.out.println();
+
+        System.out.println(intNumber + " " + doubleNumber + " " + school + " " + language);
+
+        System.out.println();
+
+        System.out.println(addNumber);
+
+        System.out.println();
+
+        System.out.println(size);
+
+        System.out.println();
+        
+        System.out.println(multiplyNumber);
+
+
+
+
+    }
+}
